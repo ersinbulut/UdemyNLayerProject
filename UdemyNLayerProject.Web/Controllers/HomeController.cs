45e00f6f8422ced508d220856c1d258490589eae
ersinbulut <ersinblt61@gmail.com>
2020-11-23 16:30:54 +0300

    57. Asp.Net Core Mvc projesinin oluşturulması

--- UdemyNLayerProject.Web/Controllers/HomeController.cs
+++ UdemyNLayerProject.Web/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace UdemyNLayerProject.Web.Controllers
 
         public HomeController(ILogger<HomeController> logger)
         {
-            _logger = logger;
+            _logger = logger;///
         }
 
         public IActionResult Index()
